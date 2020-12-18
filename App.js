c0d2ada5fbc595a2cf73b35821870de21fb477ab
kevagreen <74441751+kevagreen@users.noreply.github.com>
2020-12-18 10:36:24 -0600

    Add files via upload
    additional src files, attempted to add <Game /> to app.js and received initial error failed to compile

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Game from './components/Game';
+
+
+
+// import Game from './components/Game.js';
+
+function App() {
+  return (
+    <div className="App">
+      
+     
+    </div>
+  );
+}
+
+export default App;
